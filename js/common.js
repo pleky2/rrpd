1724bde25607e331cab749ea6f4c8b26911013d5
ony arianto <onyarianto03@gmail.com>
2024-09-18 22:35:22 +0700

    milestone

--- js/common.js
+++ js/common.js
@@ -1,9 +1,29 @@
-function tPop() {
-    document.addEventListener("DOMContentLoaded", function (e) {
-        o()(".grid-container .grid-item").on("click", function (e) {
-            e.preventDefault();
-            this.find(".data-popup").html();
-            o()("#popup-bod .content-popup").html(t), openPopup("#popup-bod");
-        });
-    });
-}
+$(document).ready(function(){
+    console.log('testing')
+    $('.our-milestone .slider-dots').slick({
+        arrows: false,
+        centerMode: true,
+        slidesToShow: 5,
+        infinite: false,
+        asNavFor: ".slider-center-content",
+        dots: false,
+        focusOnSelect: true,
+        responsive: [{ breakpoint: 767, settings: { slidesToShow: 2, variableWidth: true } }],
+    })
+
+    $('.slider-center-content').slick({ arrows: !1, slidesToShow: 1, fade: !1, dots: !1, infinite: !1, asNavFor: ".slider-dots", draggable: !1,  });
+    var e = function() {
+        $(".slider-milestone").slick({ arrows: !0, dots: !1, slidesToShow: 1, infinite: !1,  });
+    }
+    e()
+ 
+    $(".our-milestone .slider-dots").on("afterChange", function () {
+        $(".slider-milestone").slick("unslick");
+        e();
+    })
+    $(".slider-milestone").on("afterChange", function (e) {
+        console.log('asu', $(".slider-milestone .slick-prev").attr("aria-disabled"))
+        "true" == $(".slider-milestone .slick-prev").attr("aria-disabled") ? $(".arrow.prev").addClass("disabled") : $(".arrow.prev").removeClass("disabled");
+        "true" == $(".slider-milestone .slick-next").attr("aria-disabled") ? $(".arrow.next").addClass("disabled") : $(".arrow.next").removeClass("disabled");
+    })
+});
