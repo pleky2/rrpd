8a85c1732da152031dafe4d0031c6433462fe57c
ony arianto <onyarianto03@gmail.com>
2024-10-16 09:24:49 +0700

    footer

--- resources/views/template/footer.blade.php
+++ resources/views/template/footer.blade.php
@@ -1,42 +1,83 @@
   <!--Footer -->
   <footer>
-    <div class="wrapper">
-      <div class="foot-top">
-        <div class="foot-top__box logo">
+    <div class="container">
+      <div class="row">
+        <div class="column column-40">
           <a href="#" class="img-logo">
-            <img src="{{ asset('assets/images/logo_home.png') }}" alt="">
+            <img src="{{ asset('assets/images/logo-foot.jpg') }}" alt="">
           </a>
-        </div>
-        <div class="foot-top__box">
-          <h5>Kantor Utama</h5>
-          <a href="#" class="icon">
-            <img width="24" height="24" src="{{ asset('assets/images/material/ic-loc.svg') }}" alt="">
-            Palmsville Residance Blok AB-2 Lt. 2, Jagabaya III, Bandar, Kec. Way Halim, Kota Bandar Lampung, Lampung
-            35132
-          </a>
-        </div>
-        <div class="foot-top__box">
+          <div class="contact">
+            <h5>Kontak BTI</h5>
+            <p>Email : info@btiofficial.co.id</p>
+            <p>No. Telepon 	: 081385389774</p>
+          </div>
 
-          <h5>Pabrik</h5>
-          <a href="#" class="icon">
-            <img width="24" height="24" src="{{ asset('assets/images/material/ic-loc.svg') }}" alt="">
-            Srikaton, Kec. Tj. Bintang, Kabupaten Lampung Selatan, Lampung
-          </a>
-        </div>
-
-        <div class="foot-top__box">
-
-          <h5>Follow Us</h5>
+          <div>
+            <h5>Sosial Media</h5>
             <div class="media">
               <a href="#" target="_blank"><img src="{{ asset('assets/images/material/ic-ig-green.svg') }}" alt="icon"></a>
               <a href="#" target="_blank"><img src="{{ asset('assets/images/material/ic-yt-green.svg') }}" alt="icon"></a>
               <a href="#" target="_blank"><img src="{{ asset('assets/images/material/ic-lnk-green.svg') }}" alt="icon"></a>
               <a href="#" target="_blank"><img width="22" height="22" src="{{ asset('assets/images/material/fb-lnk.svg') }}" alt="icon"></a>
             </div>
+          </div>
         </div>
+        <div class="column column-70">
+          <h5>Alamat</h5>
+          <div class="row">
+            <div class="column">
+              <h5>Headquarter</h5>
+              <span>Palmsville Residance Blok AB-2 Lt. 2, Jagabaya III, Bandar, Kec. Way Halim, Kota Bandar Lampung, Lampung 35132</span>
+            </div>
+            <div class="column">
+              <h5>Pabrik Woodchips </h5>
+              <span>Desa Srikaton, Kec. Tj. Bintang, Kab. Lampung Selatan, Lampung</span>
 
+              <div class="partner">
+                <img src="{{ asset('assets/images/enpos.png') }}" alt="">
+                <img src="{{ asset('assets/images/enpos.png') }}" alt="">
+                <img src="{{ asset('assets/images/enpos.png') }}" alt="">
+                <img src="{{ asset('assets/images/enpos.png') }}" alt="">
+              </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        
       </div>
-      <div class="row">
+      <!-- <div class="foot-top">
+        <div class="foot-top__box logo">
+          <a href="#" class="img-logo">
+            <img src="{{ asset('assets/images/logo_home.png') }}" alt="">
+          </a>
+          
+          <div>
+            <h5>Sosial Media</h5>
+            <div class="media">
+              <a href="#" target="_blank"><img src="{{ asset('assets/images/material/ic-ig-green.svg') }}" alt="icon"></a>
+              <a href="#" target="_blank"><img src="{{ asset('assets/images/material/ic-yt-green.svg') }}" alt="icon"></a>
+              <a href="#" target="_blank"><img src="{{ asset('assets/images/material/ic-lnk-green.svg') }}" alt="icon"></a>
+              <a href="#" target="_blank"><img width="22" height="22" src="{{ asset('assets/images/material/fb-lnk.svg') }}" alt="icon"></a>
+            </div>
+          </div>
+        </div>
+        <div class="foot-top__box">
+          <h5>Headquarter</h5>
+          <a href="#" class="icon">
+            <img width="24" height="24" src="{{ asset('assets/images/material/ic-loc.svg') }}" alt="">
+            Srikaton, Kec. Tj. Bintang, Kabupaten Lampung Selatan, Lampung
+          </a>
+        </div>
+        <div class="foot-top__box">
+          <h5></h5>
+          <h5>Pabrik Woodchips </h5>
+          <a href="#" class="icon">
+            <img width="24" height="24" src="{{ asset('assets/images/material/ic-loc.svg') }}" alt="">
+            Srikaton, Kec. Tj. Bintang, Kabupaten Lampung Selatan, Lampung
+          </a>
+        </div>
+      </div> -->
+      <!-- <div class="row">
         <div class="column column-60">
           <div class="copyright">Copyright 2024 © Bhakti Terang Indonesia. All Rights Reserved.</div>
         </div>
@@ -47,7 +88,7 @@
             <a href="#">FAQ</a>
           </div>
         </div>
-      </div>
+      </div> -->
     </div>
   </footer>
 
