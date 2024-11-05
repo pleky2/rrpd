4c1ea467c86dbda9271032c938248d195ce77149
ony arianto <onyarianto03@gmail.com>
2024-11-05 23:20:35 +0700

    superior

--- resources/views/content/contactUs.blade.php
+++ resources/views/content/contactUs.blade.php
@@ -7,48 +7,66 @@
       <img src="{{ asset('assets/images/about/cover_w1440_h400_headline-banner-about_1366x768px_.png') }}" alt="">
     </figure>
     <figcaption>
-      <h2>Mitra</h2>
+      <h2>Hubungi Kami</h2>
     </figcaption>
   </div>
-  <div class="inner-content">
-    <div class="wrapper content-pages mitra-wrapper">
-      <div class="mitra-slider">
-        <div class="slider-item">
-          <h4 class="title">Mitra 1</h4>
-          <div class="slider-grid">
-            <img src="{{ asset('assets/images/logo_home.png') }}" />
-            <img src="{{ asset('assets/images/logo_home.png') }}" />
-            <img src="{{ asset('assets/images/logo_home.png') }}" />
-            <img src="{{ asset('assets/images/logo_home.png') }}" />
-            <img src="{{ asset('assets/images/logo_home.png') }}" />
-            <img src="{{ asset('assets/images/logo_home.png') }}" />
-            <img src="{{ asset('assets/images/logo_home.png') }}" />
-            <img src="{{ asset('assets/images/logo_home.png') }}" />
-          </div>
-        </div>
-        <div class="slider-item">
-          <h4 class="title">Mitra 2</h4>
-          <div class="slider-grid">
-            <img src="{{ asset('assets/images/gh.png') }}" />
-            <img src="{{ asset('assets/images/gh.png') }}" />
-            <img src="{{ asset('assets/images/gh.png') }}" />
-            <img src="{{ asset('assets/images/gh.png') }}" />
-          </div>
-        </div>
-        <div class="slider-item">
-          <h4 class="title">Mitra 3</h4>
-          <div class="slider-grid">
-            <img src="{{ asset('assets/images/enpos.png') }}" />
-            <img src="{{ asset('assets/images/enpos.png') }}" />
-            <img src="{{ asset('assets/images/enpos.png') }}" />
-            <img src="{{ asset('assets/images/enpos.png') }}" />
+
+
+  <div class="top-contact">
+    <div class="wrapper">
+      <div class="row">
+      <div class="column">
+          <div class="contact-wrapper">
+            <div class="contact-us">
+              <h5>Headquarter</h5>
+              <div class="item">
+                <p class="content">
+                  <span><img src="{{ asset('assets/images/material/icon-location.svg') }}" alt=""></span>Palmsville
+                  Residance Blok AB-2 Lt. 2, Jagabaya III, Bandar, Kec. Way Halim, Kota
+                  Bandar Lampung, Lampung 35132
+                </p>
+                <p class="content"><span><img src="{{ asset('assets/images/material/mail-s.svg') }}"
+                      alt=""></span> info@btiofficial.co.id</p>
+                <p class="content"><span><img src="{{ asset('assets/images/material/ic-phone.svg') }}"
+                      alt=""></span> 081385389774</p>
+                <p class="content"><span><img src="{{ asset('assets/images/material/icon-instagram.svg') }}"
+                      alt=""></span> btioffial</p>
+
+              </div>
+
+            </div>
           </div>
         </div>
       </div>
+      <div class="row" style="margin-top: 50px;">
+        <div class="column">
+          <div class="contact-wrapper">
+            <div class="contact-us">
+              <h5>Headquarter</h5>
+              <div class="item">
+                <p class="content">
+                  <span><img src="{{ asset('assets/images/material/icon-location.svg') }}" alt=""></span>Palmsville
+                  Residance Blok AB-2 Lt. 2, Jagabaya III, Bandar, Kec. Way Halim, Kota
+                  Bandar Lampung, Lampung 35132
+                </p>
+                <p class="content"><span><img src="{{ asset('assets/images/material/mail-s.svg') }}"
+                      alt=""></span> info@btiofficial.co.id</p>
+                <p class="content"><span><img src="{{ asset('assets/images/material/ic-phone.svg') }}"
+                      alt=""></span> 081385389774</p>
+                <p class="content"><span><img src="{{ asset('assets/images/material/icon-instagram.svg') }}"
+                      alt=""></span> btioffial</p>
 
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-
-
   </div>
+
+
+
+
 </section>
 @endsection
