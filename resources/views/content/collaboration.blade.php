07184adde97ffea1104d2a604d30f24f5d465a69
ony arianto <onyarianto03@gmail.com>
2024-11-24 00:06:42 +0700

    kerjasama

--- resources/views/content/collaboration.blade.php
+++ resources/views/content/collaboration.blade.php
@@ -2,49 +2,72 @@
 
 @section('content')
 <section class="middle kerjasama">
-  
+
   <div class="center kerjasama-title">
     <h5>Kerjasama</h5>
     <h6>MoU BTI-ITERA</h6>
   </div>
   <div class="wrapper kerjasama-wrapper">
 
-    <div class="card">
-      <div class="img-wrapper">
-        <img src="{{ asset('assets/images/about/cover_w1440_h400_headline-banner-about_1366x768px_.png') }}" />
+    <div>
+      <div class="card">
+        <div class="img-wrapper">
+          <img src="{{ asset('assets/images/about/cover_w1440_h400_headline-banner-about_1366x768px_.png') }}" />
+        </div>
       </div>
+      <h4>sekolah menengah pertama Kegilaan</h4>
+      <p>sekolah menengah pertama Madness adalah acara penuh semangat yang menandai akhir dari setiap kuartal tahun
+        ajaran. Para siswa dan guru, dikelompokkan dalam tim yang diberi nama sesuai dengan nama gunung berapi
+        terkenal
+        di Indonesia, bekerja sama untuk berkompetisi dalam serangkaian tantangan untuk mengumpulkan poin untuk
+        "rumah"
+        mereka, mulai dari tarik tambang hingga permainan air yang sama sekali tidak boleh ada yang kering</p>
     </div>
-    <div class="card">
-      <div class="img-wrapper">
-        <img src="{{ asset('assets/images/about/cover_w1440_h400_headline-banner-about_1366x768px_.png') }}" />
+    <div>
+      <div class="card">
+        <div class="img-wrapper">
+          <img src="{{ asset('assets/images/about/cover_w1440_h400_headline-banner-about_1366x768px_.png') }}" />
+        </div>
       </div>
+      <h4>sekolah menengah pertama Kegilaan</h4>
+      <p>sekolah menengah pertama Madness adalah acara penuh semangat yang menandai akhir dari setiap kuartal tahun
+        ajaran. Para siswa dan guru, dikelompokkan dalam tim yang diberi nama sesuai dengan nama gunung berapi
+        terkenal
+        di Indonesia, bekerja sama untuk berkompetisi dalam serangkaian tantangan untuk mengumpulkan poin untuk
+        "rumah"
+        mereka, mulai dari tarik tambang hingga permainan air yang sama sekali tidak boleh ada yang kering</p>
     </div>
-    <div class="card">
-      <div class="img-wrapper">
-        <img src="{{ asset('assets/images/about/cover_w1440_h400_headline-banner-about_1366x768px_.png') }}" />
+    <div>
+      <div class="card">
+        <div class="img-wrapper">
+          <img src="{{ asset('assets/images/about/cover_w1440_h400_headline-banner-about_1366x768px_.png') }}" />
+        </div>
       </div>
+      <h4>sekolah menengah pertama Kegilaan</h4>
+      <p>sekolah menengah pertama Madness adalah acara penuh semangat yang menandai akhir dari setiap kuartal tahun
+        ajaran. Para siswa dan guru, dikelompokkan dalam tim yang diberi nama sesuai dengan nama gunung berapi
+        terkenal
+        di Indonesia, bekerja sama untuk berkompetisi dalam serangkaian tantangan untuk mengumpulkan poin untuk
+        "rumah"
+        mereka, mulai dari tarik tambang hingga permainan air yang sama sekali tidak boleh ada yang kering</p>
     </div>
-    <div class="card">
-      <div class="img-wrapper">
-        <img src="{{ asset('assets/images/about/cover_w1440_h400_headline-banner-about_1366x768px_.png') }}" />
+    <div>
+      <div class="card">
+        <div class="img-wrapper">
+          <img src="{{ asset('assets/images/about/cover_w1440_h400_headline-banner-about_1366x768px_.png') }}" />
+        </div>
       </div>
+      <h4>sekolah menengah pertama Kegilaan</h4>
+      <p>sekolah menengah pertama Madness adalah acara penuh semangat yang menandai akhir dari setiap kuartal tahun
+        ajaran. Para siswa dan guru, dikelompokkan dalam tim yang diberi nama sesuai dengan nama gunung berapi
+        terkenal
+        di Indonesia, bekerja sama untuk berkompetisi dalam serangkaian tantangan untuk mengumpulkan poin untuk
+        "rumah"
+        mereka, mulai dari tarik tambang hingga permainan air yang sama sekali tidak boleh ada yang kering</p>
     </div>
-    <div class="card">
-      <div class="img-wrapper">
-        <img src="{{ asset('assets/images/about/cover_w1440_h400_headline-banner-about_1366x768px_.png') }}" />
-      </div>
-    </div>
-    <div class="card">
-      <div class="img-wrapper">
-        <img src="{{ asset('assets/images/about/cover_w1440_h400_headline-banner-about_1366x768px_.png') }}" />
-      </div>
-    </div>
-    <div class="card">
-      <div class="img-wrapper">
-        <img src="{{ asset('assets/images/about/cover_w1440_h400_headline-banner-about_1366x768px_.png') }}" />
-      </div>
-    </div>
-   
+    
+
+
   </div>
 </section>
 @endsection
