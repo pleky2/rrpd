4c6f55417ab121c34faab51bdbe058bf445e0aba
Wreda Prasetiyo W <wreda@landx.id>
2024-12-03 21:04:24 +0700

    fix: revisi

--- resources/views/content/organization.blade.php
+++ resources/views/content/organization.blade.php
@@ -4,7 +4,7 @@
 <section class="middle gap-bottom">
     <div class="banner-page center">
       <figure>
-        <img src="{{ asset('assets/images/about/cover_w1440_h400_headline-banner-about_1366x768px_.png') }}" alt="">
+        <img src="{{ asset('upload/images/logo/bg-bti.jpeg') }}" alt="" style="max-height: 400px;">
       </figure>
       <figcaption>
         <h1><b>{{ __('content.organ') }}</b></h1>
